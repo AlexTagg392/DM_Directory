7fb92b8cdeb9ab435436793f074a384ab1e97c3c
aTagg <christag392@gmail.com>
2020-07-21 10:17:04 -0500

    created a seachable sorting table

--- dm_directory/src/components/Table/index.js
+++ dm_directory/src/components/Table/index.js
@@ -1,55 +1,36 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import MaterialTable from "material-table";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
-
-function createData(image, firstName, lastName, email, phoneNumber) {
-  return { image, firstName, lastName, email, phoneNumber };
-}
-
-const rows = [
-  createData("https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png", "Michael", "Scott", "theScarnMan@dm.net", 6158675309),
-  createData("https://img.buzzfeed.com/buzzfeed-static/static/2017-08/9/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-22381-1502291405-3.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*", "Jim", "Halpert", "smirk@gmail.com", 5555550001),
-];
-
-export default function SimpleTable() {
-  const classes = useStyles();
-
+export default function Directory() {
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Image</TableCell>
-            <TableCell align="right">Full Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Phone Number</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                <img src={row.image} alt="Employee" width="150px" height="150px"></img>
-              </TableCell>
-              <TableCell align="right">{row.firstName +" "+ row.lastName}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.phoneNumber}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    <MaterialTable
+      title="Dunder Mifflin Employee Directory"
+      columns={[
+        { title: "Employee Image", field: "image" },
+        { title: "First Name", field: "firstName" },
+        { title: "Last Name", field: "lastName" },
+        { title: "Email", field: "email" },
+        { title: "Birth Year", field: "birthYear", type: "numeric" },
+        { title: "Phone Number", field: "phoneNumber", type: "numeric" },
+        
+      ]}
+      data={[
+        { image: <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png" alt="Employee" width="150px" height="150px"></img>, firstName: "Michael", lastName:"Scott", email: "theSCARN@gmail.com", birthYear: 1964, phoneNumber: 8675309 },
+        { image: <img src="https://vignette.wikia.nocookie.net/theoffice/images/e/e9/Character_-_JimHalpert.PNG/revision/latest?cb=20200414162003" alt="Employee" width="150px" height="150px"></img>, firstName: "Jim", lastName:"Halpert", email: "smirk@gmail.com", birthYear: 1975, phoneNumber: 5553430 },
+      ]}        
+      options={{
+        sorting: true
+      }}
+    />
+  )
 }
+
+
+// function createData(image, firstName, lastName, email, phoneNumber) {
+//   return { image, firstName, lastName, email, phoneNumber };
+// }
+
+// const rows = [
+//   createData("https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png", "Michael", "Scott", "theScarnMan@dm.net", 6158675309),
+//   createData("https://img.buzzfeed.com/buzzfeed-static/static/2017-08/9/11/asset/buzzfeed-prod-fastlane-01/sub-buzz-22381-1502291405-3.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto&output-quality=auto&output-format=auto&downsize=360:*", "Jim", "Halpert", "smirk@gmail.com", 5555550001),
+// ];
