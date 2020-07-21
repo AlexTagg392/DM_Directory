3c90c3830f0cfcd339d4a94aa1adff9480dff8b6
aTagg <christag392@gmail.com>
2020-07-21 13:18:15 -0500

    Updates

---
+++ precache-manifest.c1a0a097e9947c2bbdf376ad82d7678c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2f9a2d0b7a4c265388bf2c0a9d1f1a",
+    "url": "/DM_Directory/index.html"
+  },
+  {
+    "revision": "e4ba53ba07b3eb608eac",
+    "url": "/DM_Directory/static/css/main.08193c39.chunk.css"
+  },
+  {
+    "revision": "726050e907070fd484a2",
+    "url": "/DM_Directory/static/js/2.a4b47fc0.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/DM_Directory/static/js/2.a4b47fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ba53ba07b3eb608eac",
+    "url": "/DM_Directory/static/js/main.556a984b.chunk.js"
+  },
+  {
+    "revision": "3b6a240bfce0d861ae79",
+    "url": "/DM_Directory/static/js/runtime-main.2a2aa048.js"
+  }
+]);
